53737f4a6d01bf1d5d3ebf60246938e233582b23
Pedro Lisboa <idd_jlisboa@uolinc.com>
2022-07-10 02:21:53 -0300

    Ajustes para iniciar o desenvolvimento dos controllers e handlers

--- livroservice/src/main/kotlin/com/bibliotec/livroservice/LivroserviceApplication.kt
+++ livroservice/src/main/kotlin/com/bibliotec/livroservice/LivroserviceApplication.kt
@@ -9,3 +9,10 @@ class LivroserviceApplication
 fun main(args: Array<String>) {
 	runApplication<LivroserviceApplication>(*args)
 }
+
+//Todo: criar controllers da aplicação baseado no outro projeto bibliotec existente...
+//Todo: Criar camadas e consultas dinamicas com filtros...
+//Todo: preparar handler de exceção da aplicação
+//Todo: prepara message properties da app para o handler
+//Todo: criar o config repo para app...
+//Todo: verificar a implementação fo sleuth na app
